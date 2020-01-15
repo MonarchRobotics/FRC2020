2ea08830d9df5a60de63f4ecc3221c4ab1eefd58
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2020-01-15 10:01:30 -0700

    added xbox controller into OI.java

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+
+public class OI {
+
+    public static XboxController xboxController;
+
+    public OI(){
+        xboxController = new XboxController(0);
+
+    }
+
+    public static double deadZone (double val, double deadZone){
+        if (Math.abs(val) > deadZone){
+            if (val > 0){
+                return (val - deadZone) / (1 - deadZone);
+            } else {
+                return -(-val - deadZone) / (1 - deadZone);
+            }
+        }
+        return 0;
+    }
+}
