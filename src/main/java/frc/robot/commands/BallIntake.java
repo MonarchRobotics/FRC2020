da8faa329291de3a0cfa242485ddbb50d660388f
geb92011 <37025708+geb92011@users.noreply.github.com>
2020-01-16 08:37:10 -0700

    Added classes for ball intake

---
+++ src/main/java/frc/robot/commands/BallIntake.java
@@ -0,0 +1,90 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+
+import frc.robot.subsystems.BallSuck;
+
+
+import frc.robot.OI;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+
+/**
+ * The Pnumatic climbing system
+ */
+public class BallIntake extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final BallSuck m_subsystem;
+  
+  
+
+
+  /**
+   * @param subsystem The subsystem used by this command.
+   */
+  public BallIntake(BallSuck subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+
+
+
+    // camera = new VideoCapture(0);
+    // if(camera.isOpened()){
+    //   System.out.println("Camera is ready");
+    // }
+
+  }
+  
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    NetworkTable table = inst.getTable("myContoursReport");
+    System.out.print(table.getKeys());
+
+    // When A on the xbox controller is pressed toggle climbing
+    if (OI.xboxController.getAButtonPressed()) {
+      if (m_subsystem.getValue() != DoubleSolenoid.Value.kForward) {
+        m_subsystem.extendClimb();
+      }
+      else {
+        m_subsystem.retractClimb();
+      }
+    }
+    
+    // Mat frame = new Mat();
+    // camera.read(frame);
+
+  }
+
+  
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/BallSuck.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.BallIntake;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+//Subsystem for climbing system
+public class BallSuck extends SubsystemBase {
+
+  
+  // Motor to intake balls into the chassie
+  private TalonSRX Intake;
+
+  public BallSuck(int intake) {
+
+    Intake = new TalonSRX(intake);
+  }
+
+  // 
+  public void 
+
+  
+  // @Override
+  // public void initDefaultCommand(){
+  //   setDefaultCommand(new ExampleCommand());
+  // }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
