bad2e8228824bc8c82fcdb4233289365a6b5fb08
geb92011 <37025708+geb92011@users.noreply.github.com>
2020-01-23 17:33:27 -0700

    Added the shooter input motor to the robot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,7 +27,8 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
    private final Drivetrain drivetrain = new Drivetrain(Constants.getLeftWheelPort1(), Constants.getLeftWheelPort2(), Constants.getRightWheelPort1(), Constants.getRightWheelPort2());
    private final WheelManipulator wheelManipulator = new WheelManipulator(Constants.getWheelOfFortunePort(),6,7); //change back to constant spinnerPort
-  // private final Turret turret = new Turret(Constants.getShooterPort1(),Constants.getShooterPort2());
+  // private final Turret turret = new Turret(Constants.getShooterPort1(), Constants.getShooterPort2(), Constants.getShooterInputPort());
+   
   // private final PullUp pullup = new PullUp(0, 1);
   // private final BallSuck ballsuck = new BallSuck(Constants.getBallIntake(), Constants.getInternalManipulation());
 
