b11afc1c745c616c088f247f78506de7dea0aa26
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2020-01-29 16:29:43 -0700

    added velocity mode

--- src/main/java/frc/robot/commands/DriveTank.java
+++ src/main/java/frc/robot/commands/DriveTank.java
@@ -47,8 +47,14 @@ public class DriveTank extends CommandBase {
 //            drivetrain.ldrive(OI.joystick1.getY());
 //            drivetrain.rdrive(OI.joystick2.getY());
 
-         drivetrain.rdrive(OI.deadZone(-OI.joystick1.getY(), Constants.getRightDeadZone()));
-        drivetrain.ldrive(OI.deadZone(-OI.joystick2.getY(), Constants.getLeftDeadZone()));
+//        drivetrain.rdrive(OI.deadZone(-OI.joystick1.getY(), Constants.getRightDeadZone()));
+//        drivetrain.ldrive(OI.deadZone(-OI.joystick2.getY(), Constants.getLeftDeadZone()));
+        if(OI.joystick1.getTrigger()){
+            drivetrain.rdrive2(1);
+            drivetrain.ldrive2(1);
+        }
+
+
 
 
         //TODO: Look into differential drive
