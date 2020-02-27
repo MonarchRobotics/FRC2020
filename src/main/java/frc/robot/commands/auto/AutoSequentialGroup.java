af3a0490a289d7e4190576596b4df103f9b0caaf
geb92011 <37025708+geb92011@users.noreply.github.com>
2020-02-26 18:24:10 -0700

    Added parallel commands to the new auto group

---
+++ src/main/java/frc/robot/commands/auto/AutoSequentialGroup.java
@@ -0,0 +1,29 @@
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Turret;
+import frc.robot.subsystems.BallSuck;
+
+/**
+* Collects commands used for autonomous
+ *
+ *
+*/
+public class AutoSequentialGroup extends SequentialCommandGroup {
+    /**
+     * @param turret The Turret Subsystem {@link Turret} so that we can shoot balls
+     * @param ballsuck The BallSuck Subsystem {@link BallSuck} so that we can SUCC balls
+     * @param drivetrain The Drivetrain Subsystem {@link Drivetrain} so that we can drive!
+     * */
+    public AutoSequentialGroup(Turret turret, Drivetrain drivetrain, BallSuck ballsuck){
+        //get the ballsuck system out of frame perimeter at the start of the match
+        
+        addCommands(new ParallelCommandGroup(
+            new AutoInit(ballsuck,drivetrain),
+            new DriveStraight(drivetrain,-27,0.35),
+            new AutoShootBall(turret,28)
+        ));
+    }
+}
